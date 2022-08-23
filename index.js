65bd65b41323e6cee73c091026f05b3e501ee465
ogola5 <ogolaevance5@gmail.com>
2022-08-23 09:45:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
+  
+  return tutorials.map(str => {
+   
+ 
+     
+   
+    str = str.split(' ');
+    for (let i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    }
+    return str.join(' ');
+    
+       
+    
+      
+   
+  })
+
+  
+  
 }
+
